f137294587d4a2828c5767122ab7a164355c03dc
omnia sayed <81355933+omniaSayed@users.noreply.github.com>
2022-12-07 00:46:14 +0200

    model deployment on flask

---
+++ Speaker_recognition_model.py
@@ -0,0 +1,134 @@
+#The output of this section is the CSV files with the data to be handle by the model
+CREATE_CSV_FILES = True
+
+# Defines the names of the CSV files
+TRAIN_CSV_FILE = "Speaker_Train_File.csv"
+TEST_CSV_FILE = "Speaker_Test_File.csv"
+
+import matplotlib.pyplot as plt
+import scipy.io.wavfile
+import numpy as np
+from matplotlib import cm
+import pandas as pd
+import librosa
+import zipfile
+import csv
+import os
+from sklearn import metrics
+import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+import csv
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import zipfile as zf
+import pickle
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+files = zf.ZipFile("recordings/Spea.zip", 'r')
+files.extractall('Audio_Data')
+files.close()
+
+def extractWavFeatures(soundFilesFolder, csvFileName):
+    print("The features of the files in the folder "+soundFilesFolder+" will be saved to "+csvFileName)
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 41):
+        header += f' mfcc{i}'
+    header += ' label'
+    header = header.split()
+    print('CSV Header: ', header)
+    file = open(csvFileName, 'w', newline='')
+    #with file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    for filename in os.listdir(soundFilesFolder):
+        if filename.endswith('.wav'):
+            number = f'{soundFilesFolder}/{filename}'
+            y, sr = librosa.load(number, mono=True, duration=30)
+            # remove leading and trailing silence
+            y, index = librosa.effects.trim(y)
+            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+            rmse = librosa.feature.rms(y=y)
+            spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+            spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+            rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+            zcr = librosa.feature.zero_crossing_rate(y)
+            mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc = 40)
+            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+            for e in mfcc:
+                to_append += f' {np.mean(e)}'
+            writer.writerow(to_append.split())
+    file.close()
+    print("End of extractWavFeatures")
+
+if (CREATE_CSV_FILES == True):
+    extractWavFeatures("Audio_Data/Speaker_recognition_train_data", TRAIN_CSV_FILE)
+    extractWavFeatures("Audio_Data/Speaker_recognition_test_data", TEST_CSV_FILE)
+    print("CSV files are created")
+else:
+    print("CSV files creation is skipped")
+
+#Reading a dataset and convert file name to corresbonding umnber
+def preProcessData(csvFileName):
+    header_name_list = ['filename', 'chroma_stft', 'rmse', 'spectral_centroid', 'spectral_bandwidth', 'rolloff', 'zero_crossing_rate', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5', 'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12', 'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'mfcc17', 'mfcc18', 'mfcc19', 'mfcc20', 'mfcc21', 'mfcc22', 'mfcc23', 'mfcc24', 'mfcc25', 'mfcc26', 'mfcc27', 'mfcc28', 'mfcc29', 'mfcc30', 'mfcc31', 'mfcc32', 'mfcc33', 'mfcc34', 'mfcc35', 'mfcc36', 'mfcc37', 'mfcc38', 'mfcc39', 'mfcc40', 'label']
+    print(csvFileName+ " will be preprocessed")
+    data = pd.read_csv(csvFileName)
+    # we have 4 speakers:
+    # 0: Marina
+    # 1: Mohab
+    # 2: Omnia
+    # 3: Youssef
+    filenameArray = data['filename'] 
+    speakerArray = []
+    for filename in filenameArray:
+        if "marina" in filename:
+            speaker = 0
+        elif "mohab" in filename:
+            speaker = 1
+#          elif "omnia" in filename:
+#                speaker = "2"
+        elif "Youssef" in filename:
+             speaker = "3"
+        else: 
+            speaker = "4"
+        speakerArray.append(speaker)
+    data['number'] = speakerArray
+    #Dropping unnecessary columns
+    data = data.drop(['filename'], axis=1)
+    data = data.drop(['label'], axis=1)
+    data = data.drop(['chroma_stft'], axis=1)
+    return data
+
+trainData = preProcessData(TRAIN_CSV_FILE)
+testData = preProcessData(TEST_CSV_FILE)
+
+# Splitting the dataset into training, validation and testing dataset
+X = np.array(trainData.iloc[:, :-1], dtype = float)
+y = trainData.iloc[:, -1]
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
+X_test = np.array(testData.iloc[:, :-1], dtype = float)
+y_test = testData.iloc[:, -1]
+
+#Normalizing the dataset
+scaler = StandardScaler()
+X_train = scaler.fit_transform( X_train )
+X_val = scaler.transform( X_val )
+X_test = scaler.transform( X_test )
+
+#Create an object (model)
+dtr1 = DecisionTreeClassifier()
+
+#Fit (train) the model
+dtr1.fit(X_train, y_train)
+# print("model is fit successfully")
+#dumping the model into a .pkl file
+pickle.dump(dtr1, open('finalized_model.sav', 'wb'))
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,29 +1,65 @@
-from flask import Flask, render_template, request
-import os
-from scipy.io import wavfile
-
+from flask import Flask, render_template, request, redirect,jsonify
+import speech_recognition as sr
+import Request
+import numpy as np
+import pickle
+import librosa
 app = Flask(__name__)
+speaker_model = pickle.load(open('finalized_model.sav', 'rb'))
 
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+    return render_template('index.html', transcript=transcript)
 
 
-@app.route('/record', methods=['POST'])
-def save():
-    if request.method == 'POST':
-        file = request.files['AudioFile']
 
-        file.save(os.path.join(
-            'static/assests/records/recorded_Sound.wav'))
-        # sr, audio = wavfile.read(
-        #     'static/assets/records/assets/records/recordedAudio.wav')
-        # if len(audio.shape) > 1:
-        #     audio = audio[:, 0]
+# extract featires for a input single audio
+def extract_features_from_input(input_audio):
+    extracted_features = []
+    y, sr = librosa.load(input_audio, mono=True, duration=30)
+    y, index = librosa.effects.trim(y)
+#     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    rmse = librosa.feature.rms(y=y)
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc = 40)
+    extracted_features.extend([np.mean(rmse), np.mean(spec_cent),np.mean(spec_bw), np.mean(rolloff), np.mean(zcr)])
+    for e in mfcc:
+        extracted_features.append(np.mean(e))
+    return np.array(extracted_features)
 
-    return[]
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    audio_features = extract_features_from_input(request.files["file"])
+    final_features = audio_features.reshape(1,45)
+    prediction = speaker_model.predict(final_features)
+    speakers_list = [(0, 'Marina'), (1, 'Mohab'), (3, 'Yousef'), (4, 'Others')]
+    for iterator, speaker in enumerate(speakers_list):
+        if prediction[0] == speaker[0]:
+           return render_template('index.html', prediction_text='Predicted Speaker $ {}'.format(prediction[1]))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
+
+    
